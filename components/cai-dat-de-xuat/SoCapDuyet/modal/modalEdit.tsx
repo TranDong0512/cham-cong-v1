296a71616aaae88274edf1a414ce67192900cabc
TranDong0512 <tdong0512@gmail.com>
2023-12-13 08:59:40 +0700

    v3

--- components/cai-dat-de-xuat/SoCapDuyet/modal/modalEdit.tsx
+++ components/cai-dat-de-xuat/SoCapDuyet/modal/modalEdit.tsx
@@ -6,58 +6,57 @@ import {
   Form,
   Checkbox,
   Select,
-} from 'antd'
-import { useEffect, useState } from 'react'
-import instance from '@/components/hooks/axios.config'
-import { toast } from 'react-toastify'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
-import { RootState } from '@/redux/storeQLC'
-const { Text } = Typography
-import { openModalEdit, updateSoCapDuyet } from '../../reducer/reducer'
-import { CloseOutlined } from '@ant-design/icons'
+} from "antd";
+import { useEffect, useState } from "react";
+import instance from "@/components/hooks/axios.config";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/storeQLC";
+const { Text } = Typography;
+import { openModalEdit, updateSoCapDuyet } from "../../reducer/reducer";
+import { CloseOutlined } from "@ant-design/icons";
 export default function ModalEditSCD({
   setHidden,
   setSelectedRowKeys,
   setData,
 }) {
   const option = [
-    { value: 1, label: 'Duyệt lần lượt' },
-    { value: 2, label: 'Duyệt đồng thời' },
-    { value: 1, label: 'Duyệt lần lượt và đồng thời' },
-  ]
-  const dispatch = useDispatch()
-  const [check, setCheck] = useState(false)
-  const [propose, setPropose] = useState([])
-  const dataOld = useSelector((state: RootState) => state.tgd.dataOld)
-  console.log(dataOld)
-  const [editFieldValue, setEditFieldValue] = useState(dataOld)
+    { value: 1, label: "Duyệt lần lượt" },
+    { value: 2, label: "Duyệt đồng thời" },
+    { value: 1, label: "Duyệt lần lượt và đồng thời" },
+  ];
+  const dispatch = useDispatch();
+  const [check, setCheck] = useState(false);
+  const [propose, setPropose] = useState([]);
+  const dataOld = useSelector((state: RootState) => state.tgd.dataOld);
+  const [editFieldValue, setEditFieldValue] = useState(dataOld);
   const isUpdateSCD = useSelector(
     (state: RootState) => state.tgd.updateSoCapDuyet
-  )
-  const load = useSelector((state: RootState) => state.tgd.updateSoCapDuyet)
+  );
+  const load = useSelector((state: RootState) => state.tgd.updateSoCapDuyet);
 
-  const open = useSelector((state: RootState) => state.tgd.openModalEdit)
-  const ep_id = useSelector((state: RootState) => state.tgd.data)
+  const open = useSelector((state: RootState) => state.tgd.openModalEdit);
+  const ep_id = useSelector((state: RootState) => state.tgd.data);
 
   const handleSelect = (value) => {
     if (value) {
-      setPropose(value)
-    } else setCheck(false)
-  }
-  const [confirm_level, set_confirm_level] = useState<any>()
-  const [confirm_time, set_confirm_time] = useState<any>()
-  const [confirm_type, set_confirm_type] = useState<any>()
+      setPropose(value);
+    } else setCheck(false);
+  };
+  const [confirm_level, set_confirm_level] = useState<any>();
+  const [confirm_time, set_confirm_time] = useState<any>();
+  const [confirm_type, set_confirm_type] = useState<any>();
   const dataInput = (value, name) => {
-    if (name == 'confirm_level') set_confirm_level(+value)
-    if (name == 'confirm_time') set_confirm_time(+value)
-  }
+    if (name == "confirm_level") set_confirm_level(+value);
+    if (name == "confirm_time") set_confirm_time(+value);
+  };
   const onChange = (value: string) => {
-    set_confirm_type(+value)
-  }
+    set_confirm_type(+value);
+  };
   const onClose = () => {
-    dispatch(openModalEdit(false))
-  }
+    dispatch(openModalEdit(false));
+  };
 
   const onFinish = async () => {
     const object = {
@@ -65,17 +64,17 @@ export default function ModalEditSCD({
       confirm_level: confirm_level ? confirm_level : dataOld.confirm_level,
       confirm_type: confirm_type ? confirm_type : dataOld.confirm_type,
       confirm_time: confirm_time ? confirm_time : dataOld.confirm_time,
-    }
+    };
     const response = await instance.post(
-      'api/qlc/settingConfirm/settingPropose',
+      "api/qlc/settingConfirm/settingPropose",
       object
-    )
+    );
     if (response?.status == 200) {
-      toast.success('Chỉnh sửa thành công')
-      onClose()
-      dispatch(updateSoCapDuyet(!load))
+      toast.success("Chỉnh sửa thành công");
+      onClose();
+      dispatch(updateSoCapDuyet(!load));
     }
-  }
+  };
   return (
     <>
       <Modal
@@ -83,135 +82,147 @@ export default function ModalEditSCD({
         // wrapClassName="CustomerModal"
         footer={false}
         onCancel={onClose}
-        closeIcon={<CloseOutlined style={{ color: '#fff' }} />}>
-        <Form name='form' onFinish={onFinish}>
+        closeIcon={<CloseOutlined style={{ color: "#fff" }} />}
+      >
+        <Form name="form" onFinish={onFinish}>
           <div
-            className='px-24 py-16'
+            className="px-24 py-16"
             style={{
-              backgroundColor: '#4C5BD4',
-              borderRadius: '8px 8px 0 0',
-            }}>
+              backgroundColor: "#4C5BD4",
+              borderRadius: "8px 8px 0 0",
+            }}
+          >
             <p
-              className='color-white font-size-18'
-              style={{ padding: '10px', color: '#fff' }}>
+              className="color-white font-size-18"
+              style={{ padding: "10px", color: "#fff" }}
+            >
               Chỉnh sửa đề xuất
             </p>
           </div>
-          <div style={{ padding: '8px 20px' }}>
-            <div className='mt-16'>
-              <Typography.Text style={{ fontSize: '16px', fontWeight: 500 }}>
+          <div style={{ padding: "8px 20px" }}>
+            <div className="mt-16">
+              <Typography.Text style={{ fontSize: "16px", fontWeight: 500 }}>
                 <p> Danh sách đề xuất</p>
               </Typography.Text>
             </div>
 
-            <div className='mt-8' style={{ marginTop: '8px' }}>
+            <div className="mt-8" style={{ marginTop: "8px" }}>
               <Select
                 disabled
-                placeholder='Chọn'
+                placeholder="Chọn"
                 onChange={handleSelect}
                 defaultValue={`${dataOld?.dexuat_name}`}
                 onClear={() => {
-                  setCheck(false)
+                  setCheck(false);
                 }}
                 allowClear
                 showSearch
                 style={{
-                  width: '100%',
+                  width: "100%",
                 }}
                 filterOption={(input, option: any) =>
-                  (option?.label ?? '').includes(input)
+                  (option?.label ?? "").includes(input)
                 }
-                size='large'
+                size="large"
                 options={setData}
               />
             </div>
 
-            <div className='mt-16'>
+            <div className="mt-16">
               <Typography.Text
                 style={{
-                  fontSize: '16px',
+                  fontSize: "16px",
                   fontWeight: 400,
-                }}>
+                }}
+              >
                 Số cấp duyệt
               </Typography.Text>
             </div>
             <InputNumber
-              placeholder='Nhập số cấp duyệt'
-              size='large'
-              style={{ marginTop: '8px', width: '100%' }}
-              onChange={(e) => dataInput(e, 'confirm_level')}
-              defaultValue={dataOld?.confirm_level}></InputNumber>
+              placeholder="Nhập số cấp duyệt"
+              size="large"
+              style={{ marginTop: "8px", width: "100%" }}
+              onChange={(e) => dataInput(e, "confirm_level")}
+              defaultValue={dataOld?.confirm_level}
+            ></InputNumber>
 
-            <div className='mt-16' style={{ marginTop: '8px' }}>
+            <div className="mt-16" style={{ marginTop: "8px" }}>
               <Typography.Text
                 style={{
-                  fontSize: '16px',
+                  fontSize: "16px",
                   fontWeight: 400,
-                }}>
+                }}
+              >
                 Hình thức duyệt
               </Typography.Text>
             </div>
             <Select
-              style={{ width: '100%' }}
-              size='large'
+              style={{ width: "100%" }}
+              size="large"
               onChange={onChange}
-              placeholder='Chọn hình thức duyệt'
+              placeholder="Chọn hình thức duyệt"
               defaultValue={option[dataOld.confirm_type - 1].label}
               options={[
                 {
-                  value: '1',
-                  label: 'Duyệt lần lượt',
+                  value: "1",
+                  label: "Duyệt lần lượt",
                 },
                 {
-                  value: '2',
-                  label: 'Duyệt đồng thời',
+                  value: "2",
+                  label: "Duyệt đồng thời",
                 },
                 {
-                  value: '3',
-                  label: 'Duyệt lần lượt và đồng thời',
+                  value: "3",
+                  label: "Duyệt lần lượt và đồng thời",
                 },
-              ]}></Select>
-            <div className='mt-16' style={{ marginTop: '8px' }}>
+              ]}
+            ></Select>
+            <div className="mt-16" style={{ marginTop: "8px" }}>
               <Typography.Text
                 style={{
-                  fontSize: '16px',
+                  fontSize: "16px",
                   fontWeight: 400,
-                }}>
+                }}
+              >
                 Giới hạn thời gian duyệt
               </Typography.Text>
             </div>
             <InputNumber
-              placeholder='Nhập thời gian'
-              size='large'
-              style={{ marginTop: '8px', width: '100%' }}
-              onChange={(e) => dataInput(e, 'confirm_time')}
-              addonAfter='Giờ'
-              defaultValue={dataOld?.confirm_time}></InputNumber>
+              placeholder="Nhập thời gian"
+              size="large"
+              style={{ marginTop: "8px", width: "100%" }}
+              onChange={(e) => dataInput(e, "confirm_time")}
+              addonAfter="Giờ"
+              defaultValue={dataOld?.confirm_time}
+            ></InputNumber>
           </div>
           <div
-            className='mt-16'
+            className="mt-16"
             style={{
-              paddingBottom: '32px',
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              paddingTop: '12px',
-              width: '75%',
-              margin: '0 auto',
-            }}>
+              paddingBottom: "32px",
+              display: "flex",
+              justifyContent: "space-evenly",
+              paddingTop: "12px",
+              width: "75%",
+              margin: "0 auto",
+            }}
+          >
             <Button
-              style={{ padding: '4px 48px', height: '38px' }}
-              onClick={onClose}>
+              style={{ padding: "4px 48px", height: "38px" }}
+              onClick={onClose}
+            >
               Hủy
             </Button>
             <Button
-              type='primary'
-              style={{ padding: '4px 48px', height: '36px' }}
-              htmlType='submit'>
+              type="primary"
+              style={{ padding: "4px 48px", height: "36px" }}
+              htmlType="submit"
+            >
               <p> Lưu</p>
             </Button>
           </div>
         </Form>
       </Modal>
     </>
-  )
+  );
 }
--- pages/quan-ly/lich-lam-viec/index.jsx
+++ pages/quan-ly/lich-lam-viec/index.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { Calendar } from 'antd'
-import dayjs from 'dayjs'
-import 'dayjs/locale/vi'
-import styles from './index.module.css'
-import axios from 'axios'
-import _ from 'lodash'
-import cookieCutter from 'cookie-cutter'
-import { domain } from '../../cai-dat-luong/hoa-hong/hoahongdoanhthu/AddTable'
-import jwtDecode from 'jwt-decode'
+import React, { useEffect, useState } from "react";
+import { Calendar, ConfigProvider } from "antd";
+import dayjs from "dayjs";
+import "dayjs/locale/vi";
+import styles from "./index.module.css";
+import axios from "axios";
+import _ from "lodash";
+import cookieCutter from "cookie-cutter";
+import { domain } from "../../cai-dat-luong/hoa-hong/hoahongdoanhthu/AddTable";
+import jwtDecode from "jwt-decode";
 
-dayjs.locale('vi')
+dayjs.locale("vi");
 
 const List = () => {
-  const [apiData, setApiData] = useState([])
-  const [selectedMonth, setSelectedMonth] = useState(dayjs().month() + 1)
-  const [selectedYear, setSelectedYear] = useState(dayjs().year())
-  const [user, setUser] = useState([])
+  const [apiData, setApiData] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState(dayjs().month() + 1);
+  const [selectedYear, setSelectedYear] = useState(dayjs().year());
+  const [user, setUser] = useState([]);
   useEffect(() => {
-    fetchApiData(selectedMonth, selectedYear)
-  }, [selectedMonth, selectedYear])
+    fetchApiData(selectedMonth, selectedYear);
+  }, [selectedMonth, selectedYear]);
 
-  const token = cookieCutter.get('token_base365')
+  const token = cookieCutter.get("token_base365");
   const fetchApiData = (month, year) => {
     const start_date = `${year}-${String(month).padStart(
       2,
-      '0'
-    )}-01T00:00:00.000+00:00`
+      "0"
+    )}-01T00:00:00.000+00:00`;
     const end_date = dayjs(start_date)
-      .add(1, 'month')
-      .format('YYYY-MM-01T00:00:00.000+00:00')
+      .add(1, "month")
+      .format("YYYY-MM-01T00:00:00.000+00:00");
 
-    const decoded = jwtDecode(token)?.['data']
-    const idQLC = decoded?.['idQLC']
-    const cp = decoded?.['com_id']
+    const decoded = jwtDecode(token)?.["data"];
+    const idQLC = decoded?.["idQLC"];
+    const cp = decoded?.["com_id"];
 
     axios
       .post(`${domain}/api/tinhluong/nhanvien/qly_ttnv`, {
@@ -45,17 +45,17 @@ const List = () => {
         end_date: end_date,
       })
       .then((response) => {
-        setApiData(response.data)
+        setApiData(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching data from API:', error)
-      })
-  }
+        console.error("Error fetching data from API:", error);
+      });
+  };
 
   const fetchApiUser = () => {
-    const decoded = jwtDecode(token)?.['data']
-    const idQLC = decoded?.['idQLC']
-    const cp = decoded?.['com_id']
+    const decoded = jwtDecode(token)?.["data"];
+    const idQLC = decoded?.["idQLC"];
+    const cp = decoded?.["com_id"];
 
     axios
       .post(`${domain}/api/tinhluong/nhanvien/qly_ho_so_ca_nhan`, {
@@ -64,52 +64,52 @@ const List = () => {
         cp: cp,
       })
       .then((response) => {
-        setUser(response.data?.data?.info_dep_com?.user)
+        setUser(response.data?.data?.info_dep_com?.user);
       })
       .catch((error) => {
-        console.error('Error fetching data from API:', error)
-      })
-  }
+        console.error("Error fetching data from API:", error);
+      });
+  };
 
   useEffect(() => {
-    fetchApiUser()
-  }, [])
+    fetchApiUser();
+  }, []);
   const DateCellRender = ({ value }) => {
-
     return (
-      <ul className='events'>
+      <ul className="events">
         {value &&
           value?.map((item) => (
             <li
               key={item?.detail?.shift_name}
               className={styles.border_list}
-              style={{ background: '#D0EAE7' }}>
-              {item.type === 'success'}
+              style={{ background: "#D0EAE7" }}
+            >
+              {item.type === "success"}
               <span className={styles.span_abc}>{item.detail?.shift_name}</span>
             </li>
           ))}
       </ul>
-    )
-  }
+    );
+  };
   const cellRender = (value) => {
-    const valueDate = value?.format('YYYY-MM-DD')
+    const valueDate = value?.format("YYYY-MM-DD");
     const map_data_obj = _.groupBy(
       apiData?.data?.list_cycle,
-      (item) => item?.date && dayjs(item?.date)?.format('YYYY-MM-DD')
-    )
+      (item) => item?.date && dayjs(item?.date)?.format("YYYY-MM-DD")
+    );
 
     return (
-      <div className='events-container'>
+      <div className="events-container">
         <DateCellRender value={map_data_obj && map_data_obj?.[valueDate]} />
       </div>
-    )
-  }
+    );
+  };
   const onPanelChange = (value, mode) => {
-    const selectedMonth = value.month() + 1 
-    const selectedYear = value.year()
-    setSelectedMonth(selectedMonth)
-    setSelectedYear(selectedYear) 
-  }
+    const selectedMonth = value.month() + 1;
+    const selectedYear = value.year();
+    setSelectedMonth(selectedMonth);
+    setSelectedYear(selectedYear);
+  };
 
   return (
     <div style={{ flex: 1 }}>
@@ -120,7 +120,8 @@ const List = () => {
               Lịch làm việc tháng
               <a
                 className={styles.btn_cv}
-                href='/van-thu-luu-tru/trang-quan-ly-de-xuat/de-xuat-lich-lam-viec'>
+                href="/van-thu-luu-tru/trang-quan-ly-de-xuat/de-xuat-lich-lam-viec"
+              >
                 Đề xuất lịch làm việc
               </a>
             </h3>
@@ -129,7 +130,7 @@ const List = () => {
           <div className={styles.ctn}>
             <p>
               Họ và tên:
-              <span style={{ color: 'blue' }}> {user?.userName}</span>
+              <span style={{ color: "blue" }}> {user?.userName}</span>
             </p>
             <p>
               <span>ID: {user?.idQLC}</span>
@@ -137,21 +138,24 @@ const List = () => {
           </div>
         </div>
         <div className={styles.calendar}>
-          <Calendar
-            cellRender={cellRender}
-            onPanelChange={onPanelChange}
-            locale={{
-              lang: {
-                locale: 'vi',
-              },
-            }}
-            mode='month'
-            className={`${styles.centeredCalendar} centered-calendar`}
-          />
+         
+            <Calendar
+              cellRender={cellRender}
+              onPanelChange={onPanelChange}
+              locale={{
+                lang: {
+                  locale: "vi_VN",
+                  month: "Tháng",
+                  year: "Năm",
+                },
+              }}
+              className={`${styles.centeredCalendar} centered-calendar`}
+            />
+      
         </div>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
